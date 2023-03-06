27ab9b509b6a924171b26e9ae1aea28548c49993
Romildo Oliveira Souza Júnior <mildo@mildo.dev>
2023-03-06 00:03:47 -0300

    Solve: Online Judge: 10071 - Back to High School Physics

---
+++ Online Judge/10071 - Back to High School Physics/10071 - Back to High School Physics.cpp
@@ -0,0 +1,20 @@
+// Copyright 2023 Romildo Oliveira Souza Júnior <mildo@mildo.dev>
+
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;  // NOLINT(build/namespaces)
+#define ENABLE_FAST_STREAM ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+#define DEBUG(x) cerr << __LINE__ << ": [" << #x << "] = " << (x) << endl;
+// clang-format on
+
+int main() {
+  ENABLE_FAST_STREAM
+  int v, t;
+
+  while (cin >> v >> t) {
+    cout << 2 * t * v << endl;
+  }
+
+  return 0;
+}
