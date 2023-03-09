6e7257304c5e76db14b7f836b2e6dafe96db2462
Romildo Oliveira Souza Júnior <mildo@mildo.dev>
2023-03-09 20:43:08 -0300

    Solve: Online Judge: 11044 - Searching for Nessy

---
+++ Online Judge/11044 - Searching for Nessy/11044 - Searching for Nessy.cpp
@@ -0,0 +1,22 @@
+// Copyright 2023 Romildo Oliveira Souza Júnior <mildo@mildo.dev>
+
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;  // NOLINT(build/namespaces)
+#define ENABLE_FAST_STREAM ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+#define DEBUG(x) cerr << __LINE__ << ": [" << #x << "] = " << (x) << endl;
+// clang-format on
+
+int main() {
+  ENABLE_FAST_STREAM
+  int tc, n, m;
+
+  cin >> tc;
+
+  while (cin >> n >> m) {
+    cout << (n / 3) * (m / 3) << endl;
+  }
+
+  return 0;
+}
