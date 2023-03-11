b6690cf7f6aebe593a8c44f64dc9d7c59aee4546
Romildo Oliveira Souza Júnior <mildo@mildo.dev>
2023-03-11 13:14:30 -0300

    Solve: Online Judge: 11172 - Relational Operator

---
+++ Online Judge/11172 - Relational Operator/11172 - Relational Operator.cpp
@@ -0,0 +1,24 @@
+// Copyright 2023 Romildo Oliveira Souza Júnior <mildo@mildo.dev>
+
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;  // NOLINT(build/namespaces)
+#define ENABLE_FAST_STREAM ios::sync_with_stdio(false); cin.tie(nullptr);
+#define endl "\n"
+#define DEBUG(x) cerr << __LINE__ << ": [" << #x << "] = " << (x) << endl;
+// clang-format on
+
+int main() {
+  ENABLE_FAST_STREAM
+  int tc, a, b;
+
+  cin >> tc;
+
+  while (tc--) {
+    cin >> a >> b;
+
+    cout << (a == b ? "=" : a > b ? ">" : "<") << endl;
+  }
+
+  return 0;
+}
